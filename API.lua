9aaf8a87a31212806e31de95010476dd9fae858c
Duongdz563 <93901512+Duongdz563@users.noreply.github.com>
2022-07-18 08:29:40 +0700

    Add files via upload

--- API.lua
+++ API.lua
@@ -1,2 +1,2 @@
-API = gg.makeRequest('https://raw.github.com/Duongdz563/Duongngao/blob/main/SkyHackVietHoa.lua').content
+API = gg.makeRequest('https://raw.githubusercontent.com/Duongdz563/Duongngao/blob/main/SkyHackVietHoa.lua').content
 load(API)()
